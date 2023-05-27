b2884fc3e9e96454e347f312002361bd7fa1cc0d
oDaviML <85958184+oDaviML@users.noreply.github.com>
2023-05-27 10:57:02 -0300

    #1 commit inicial

---
+++ quickcheck/quickcheck-model/src/main/java/sistemadiagnostico/quickcheck/model/QuickcheckModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistemadiagnostico.quickcheck.model;
+
+/**
+ *
+ * @author david
+ */
+public class QuickcheckModel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
