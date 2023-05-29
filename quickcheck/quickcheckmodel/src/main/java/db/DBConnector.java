e0d2c8ada9babc1ceb648080c337725e0c9047f3
João Pedro Torres <>
2023-05-28 22:09:30 -0300

    Conexão com Banco de Dados - CadastroMédico - DTO/DAO/Service

--- quickcheck/quickcheckmodel/src/main/java/db/DBConnector.java
+++ quickcheck/quickcheckmodel/src/main/java/db/DBConnector.java
@@ -9,16 +9,22 @@ public class DBConnector {
 
     private DBConnector(){}
 
-
+    // Strings com informações para conexão ao BD
+    private static final String URL = "jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_aa28e844ee07386?autoReconnect=true&useSSL=false";
+    private static final String USUARIO = "b918cc3160b707";
+    private static final String SENHA = "16d9b4b7";
+    
+    // Tenta estabelecer conexão com o banco de dados.
     public static Connection getConexao() throws SQLException, ClassNotFoundException {
         try {
            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_aa28e844ee07386?autoReconnect=true&useSSL=false", "b918cc3160b707", "16d9b4b7");
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 	}
-        
+    
+    // Fecha conexão com o banco de dados imediatamente, ao invés de esperar o conector realizar a tarefa.
     public static void fecharConexao (Connection conexao, Statement comando) throws SQLException{
         if(comando != null) {
             comando.close();
